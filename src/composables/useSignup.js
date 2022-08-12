46db66fab58fbaf86041a86ee67e0348b67ef2db
zinmarmyint <zmar9251@gmail.com>
2022-08-12 17:49:38 +0630

    show error message on sign up form

--- src/composables/useSignup.js
+++ src/composables/useSignup.js
@@ -11,7 +11,7 @@ let createAccount=async(email,password,displayName)=>{
         return res
         }catch(err){
             error.value=err.message;
-            console.log(error.value)
+            
         }         
     
 }
