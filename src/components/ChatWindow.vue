7298cc184471027165e7f85c0485ce09e88b9acf
zinmarmyint <zmar9251@gmail.com>
2022-08-14 16:46:15 +0630

    chatwindow component and style it

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="chat-window">
+    <div class="messages">
+        <div class="single">
+            <span class="created_at">3mins ago</span>
+            <span class="name">zmm</span>
+            <span class="message">Hi there</span>
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.chat-window{
+    background: #fafafa;
+    padding: 30px 20px;
+}
+.single{
+    margin: 18px 0;
+}
+.created_at{
+    display: block;
+    color: #999;
+    font-size: 12px;
+    margin-bottom: 4px;
+}
+.name{
+    font-weight: bold;
+    margin-right: 6px;
+}
+.messages{
+    max-height: 400px;
+    overflow: auto;
+}
+</style>
