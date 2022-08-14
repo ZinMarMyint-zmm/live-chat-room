4f97de2a24de59175fb0197ebef223a52b4d9777
zinmarmyint <zmar9251@gmail.com>
2022-08-14 17:58:24 +0630

    realtime firebase Listener

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="chat-window">
     <div class="messages">
-        <div class="single">
-            <span class="created_at">3mins ago</span>
-            <span class="name">zmm</span>
-            <span class="message">Hi there</span>
+        <div class="single" v-for="message in messages" :key="message.id">
+            <span class="created_at">{{message.created_at}}</span>
+            <span class="name">{{message.name}}</span>
+            <span class="message">{{message.message}}</span>
 
         </div>
+        
     </div>
   </div>
 </template>
 
 <script>
+import { db } from '@/firebase/config'
+import { ref } from '@vue/reactivity'
 export default {
-
+    setup(){
+        let messages = ref([]);
+        db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
+            let results=[];
+            snap.docs.forEach((doc)=>{
+                let document = {...doc.data(),id:doc.id}
+                results.push(document);
+                // console.log(doc.data())
+            })
+            messages.value = results;
+        })
+        return {messages};
+    }
 }
 </script>
 
