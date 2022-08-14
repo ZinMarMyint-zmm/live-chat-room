540f3f8e1294654b353690fdeb73d379f98e88e0
zinmarmyint <zmar9251@gmail.com>
2022-08-14 16:06:43 +0630

    create new chat form

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="container">
     <Navbar></Navbar>
+    <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
+import NewChatForm from '../components/NewChatForm'
 import { watch } from '@vue/runtime-core'
 import Navbar from '../components/Navbar'
 import getUser from '../composables/getUser'
 import { useRouter } from 'vue-router'
 
 export default {
-  components: { Navbar },
+  components: {
+    NewChatForm, Navbar },
   setup(){
     let router = useRouter();
     let {user} = getUser();
